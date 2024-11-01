13afaa91d70cce58d8e703efd6c04cdd36b888b4
Eryq14 <155892720+Eryq14@users.noreply.github.com>
2024-11-01 18:18:41 +0100

    Delete ochrona_dostepu directory

--- ochrona_dostepu/app/ochrona/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// Sprawdzamy, czy użytkownik jest zalogowany
-if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
-    header('Location: login.php'); // Jeśli nie, przekierowujemy na stronę logowania
-    exit;
-}
-?>
--- ochrona_dostepu/app/ochrona/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start(); // Rozpoczęcie sesji
-session_destroy(); // Zniszczenie sesji
-header('Location: /projekt_kantor/app/ochrona/login.php'); // Przekierowanie do logowania
-exit;
-?>
-
